c7da59958e8919cc44ee846f55da8ea2e93a148f
Tianpeng Zhang <tzhang@g.harvard.edu>
2022-04-10 15:19:42 -0400

    Replotting Figure 2(a)-2(d). Compare x, u error of Safe Learning and constrained DAP.

--- Plotting.py
+++ Plotting.py
@@ -1,5 +1,5 @@
 import numpy as np
-def plot_with_errorband(ax,trial_data,indices = None,label='',stepped=False):
+def plot_with_errorband(ax,trial_data,indices = None,label='',stepped=False,color = None):
     '''
         Depending on whether we want to plot step function curves or linearly-iterploated curves, 
         set stepped as True or False.
@@ -17,16 +17,33 @@ def plot_with_errorband(ax,trial_data,indices = None,label='',stepped=False):
         std = np.repeat(std,2)[1:]
         indices = np.repeat(indices,2)[:-1]
    
-    ax.plot(indices,np.array(mu),label=label)
-    ax.fill_between(indices,mu+std,mu-std,alpha=0.2)
+    ax.plot(indices,np.array(mu),label=label,color=color)
+    ax.fill_between(indices,mu+std,mu-std,alpha=0.2,color = color)
 
-def plot_with_percentile(ax,trial_data,l_percentile,u_percentile,indices = None,label=''):
+def plot_with_percentile(ax,trial_data,l_percentile,u_percentile,indices = None,label='',color = None):
+    '''
+        Plot solid median curve, plus transparent [l_percentile, u_percentile] region.
+    '''
+    
     median = np.median(trial_data,axis=0)
 
     if indices is None:
         indices = np.arange(len(median))
     
-    ax.plot(indices,np.array(median),label=label)
+    ax.plot(indices,np.array(median),label=label,color = color)
     ax.fill_between(indices,np.percentile(trial_data,l_percentile,axis=0),\
                     np.percentile(trial_data,u_percentile,axis=0)\
-                    ,alpha=0.2)
+                    ,alpha=0.2,color = color)
+
+def plot_percentile(ax,trial_data,percentile,indices = None,label='',color = None):
+    '''
+        Plot solid percentile curve.
+    '''
+    
+    perc = np.percentile(trial_data,percentile,axis=0)
+
+    if indices is None:
+        indices = np.arange(len(perc))
+    
+    ax.plot(indices,perc,label=label,color = color)
+   
